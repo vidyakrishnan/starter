892098213ddde8bc77c2466978d40f2576b45d00
vidyakrishnan <43365795+vidyakrishnan@users.noreply.github.com>
2018-09-18 14:37:06 +0530

    Create set19.c

---
+++ set19.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[10],n,k,sum=0;
+clrscr();
+scanf("%d %d",&n,&k);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i])
+}
+for(i=1;i<=k;i++)
+{
+sum=sum+a[i]);
+}
+print("sum");
+getch();
+}
